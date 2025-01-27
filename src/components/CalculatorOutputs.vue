7a8984daf9293f9e7df8c2dbe7e3d259d5af0173
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2025-01-27 22:31:49 +0000

    Use dant-fns

--- src/components/CalculatorOutputs.vue
+++ src/components/CalculatorOutputs.vue
@@ -1,73 +1,90 @@
 <template>
-  <div class="flex flex-col items-center gap-4 p-8 bg-slate-100">
-    <Chart
-        :currency="temporaryCalculation?.formData?.loanCurrency || 'gbp'"
-        :interest="chartNumbers?.interest"
-        :principle="chartNumbers?.principle"
-        class="pb-8 w-full border-b border-zinc-300"
-    />
+    <div class="flex flex-col items-center gap-4 p-8 bg-slate-100">
+        <Chart
+            class="pb-8 w-full border-b border-zinc-300"
+            :currency="temporaryCalculation?.formData?.loanCurrency || 'gbp'"
+            :interest="chartNumbers?.interest"
+            :principle="chartNumbers?.principle"
+        />
 
-    <div
-        class="flex flex-wrap justify-center w-full"
-    >
-      <p
-          v-for="(item, index) in summaryData"
-          :key="`summary_${index}`"
-          class="flex flex-col gap-1 basis-[50%] p-4 text-center max-w-3xs"
-      >
-        <span class="text-xl font-bold">
-          {{ item.text }}
-        </span>
-        {{ item.value }}
-      </p>
+        <div class="flex flex-wrap justify-center w-full">
+            <p
+                v-for="(item, index) in summaryData"
+                :key="`summary_${index}`"
+                class="flex flex-col gap-1 basis-[50%] p-4 text-center max-w-3xs"
+            >
+                <span class="text-xl font-bold">
+                    {{ item.text }}
+                </span>
+                {{ item.value }}
+            </p>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import {computed} from "vue";
-import Chart from "@Components/Chart.vue";
-import { temporaryCalculation } from "@Composables/useCalculator.js";
-import {formatCurrency} from "@Utils/formatCurrency.js";
+import { computed } from 'vue'
+import { format } from 'date-fns'
+import Chart from '@Components/Chart.vue'
+import { temporaryCalculation } from '@Composables/useCalculator.js'
+import { formatCurrency } from '@Utils/formatCurrency.js'
 
 const summaryData = computed(() => {
-  return {
-    dailyInterestNoMargin: {
-      text: 'Daily Interest (no margin)',
-      value: temporaryCalculation.value?.dailyInterestNoMargin ? formatCurrency(
-          temporaryCalculation.value?.dailyInterestNoMargin,
-          temporaryCalculation.value?.formData?.loanCurrency || 'gbp'
-      ) : '-'
-    },
-    dailyInterest: {
-      text: 'Daily Interest (accrued)',
-      value: temporaryCalculation.value?.dailyInterest ? formatCurrency(
-          temporaryCalculation.value?.dailyInterest || 0,
-          temporaryCalculation.value?.formData?.loanCurrency || 'gbp'
-      ) : '-'
-    },
-    accrualDate: {
-      text: 'Accrual Date',
-      value: temporaryCalculation.value?.accrualDate || '-'
-    },
-    daysElapsed: {
-      text: 'Days Since Start',
-      value: temporaryCalculation.value?.daysSinceStart ? `${temporaryCalculation.value?.daysSinceStart} days` : '-'
-    },
-    totalInterest: {
-      text: 'Total Interest',
-      value: temporaryCalculation.value?.totalInterest ? formatCurrency(
-          temporaryCalculation.value?.totalInterest,
-          temporaryCalculation.value?.formData?.loanCurrency || 'gbp'
-      ) : '-'
+    return {
+        dailyInterestNoMargin: {
+            text: 'Daily Interest (no margin)',
+            value: temporaryCalculation.value?.dailyInterestNoMargin
+                ? formatCurrency(
+                      temporaryCalculation.value?.dailyInterestNoMargin,
+                      temporaryCalculation.value?.formData?.loanCurrency ||
+                          'gbp'
+                  )
+                : '-'
+        },
+        dailyInterest: {
+            text: 'Daily Interest (accrued)',
+            value: temporaryCalculation.value?.dailyInterest
+                ? formatCurrency(
+                      temporaryCalculation.value?.dailyInterest || 0,
+                      temporaryCalculation.value?.formData?.loanCurrency ||
+                          'gbp'
+                  )
+                : '-'
+        },
+        accrualDate: {
+            text: 'Accrual Date',
+            value: temporaryCalculation.value?.accrualDate
+                ? format(
+                      new Date(temporaryCalculation.value?.accrualDate),
+                      'do MMM yyyy'
+                  )
+                : '-'
+        },
+        daysElapsed: {
+            text: 'Days Since Start',
+            value: temporaryCalculation.value?.daysSinceStart
+                ? `${temporaryCalculation.value?.daysSinceStart} days`
+                : '-'
+        },
+        totalInterest: {
+            text: 'Total Interest',
+            value: temporaryCalculation.value?.totalInterest
+                ? formatCurrency(
+                      temporaryCalculation.value?.totalInterest,
+                      temporaryCalculation.value?.formData?.loanCurrency ||
+                          'gbp'
+                  )
+                : '-'
+        }
     }
-  }
 })
 
 const chartNumbers = computed(() => {
-  return {
-    principle: Number(temporaryCalculation.value?.formData?.loanAmount ?? 0),
-    interest: Number(temporaryCalculation.value?.totalInterest ?? 0),
-  }
+    return {
+        principle: Number(
+            temporaryCalculation.value?.formData?.loanAmount ?? 0
+        ),
+        interest: Number(temporaryCalculation.value?.totalInterest ?? 0)
+    }
 })
 </script>
--- src/pages/History.vue
+++ src/pages/History.vue
@@ -1,107 +1,137 @@
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold mb-4">History</h1>
-    <div
-        v-if="calculatorHistory?.length"
-        class="w-full min-w-full overflow-x-scroll"
-    >
-      <table class="w-full table-auto border-collapse border border-zinc-300 rounded-lg">
-        <thead>
-        <tr :class="headerRowClasses">
-          <th v-for="header in headers" :key="header" :class="cellClasses">
-            {{ header }}
-          </th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr
-            v-for="item in calculatorHistory"
-            :key="item.id"
-            :class="rowClasses"
+    <div class="p-6">
+        <h1 class="text-2xl font-bold mb-4">History</h1>
+        <div
+            v-if="calculatorHistory?.length"
+            class="w-full min-w-full overflow-x-scroll"
         >
-          <td :class="cellClasses">{{ formatDate(item.created) }}</td>
-          <td :class="cellClasses">
-            {{ formatCurrency(item.dailyInterestNoMargin, currencyLookup?.[item.formData.loanCurrency]?.locale) }}
-          </td>
-          <td :class="cellClasses">
-            {{ formatCurrency(item.dailyInterest, currencyLookup?.[item.formData.loanCurrency]?.locale) }}
-          </td>
-          <td :class="cellClasses">{{ formatDate(item.accrualDate) }}</td>
-          <td :class="cellClasses">{{ item.daysSinceStart }} days</td>
-          <td :class="cellClasses">
-            {{ formatCurrency(item.totalInterest, currencyLookup?.[item.formData.loanCurrency]?.locale) }}
-          </td>
-          <td :class="cellClasses">
-            <Button
-                button-type="text"
-                size="small"
-                :href="`/calculator/${item.id}`"
+            <table
+                class="w-full table-auto border-collapse border border-zinc-300 rounded-lg"
             >
-              View
+                <thead>
+                    <tr :class="headerRowClasses">
+                        <th
+                            v-for="header in headers"
+                            :key="header"
+                            :class="cellClasses"
+                        >
+                            {{ header }}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="item in calculatorHistory"
+                        :key="item.id"
+                        :class="rowClasses"
+                    >
+                        <td :class="cellClasses">
+                            {{ formatDate(item.created) }}
+                        </td>
+                        <td :class="cellClasses">
+                            {{
+                                formatCurrency(
+                                    item.dailyInterestNoMargin,
+                                    currencyLookup?.[item.formData.loanCurrency]
+                                        ?.locale
+                                )
+                            }}
+                        </td>
+                        <td :class="cellClasses">
+                            {{
+                                formatCurrency(
+                                    item.dailyInterest,
+                                    currencyLookup?.[item.formData.loanCurrency]
+                                        ?.locale
+                                )
+                            }}
+                        </td>
+                        <td :class="cellClasses">
+                            {{ formatDate(item.accrualDate) }}
+                        </td>
+                        <td :class="cellClasses">
+                            {{ item.daysSinceStart }} days
+                        </td>
+                        <td :class="cellClasses">
+                            {{
+                                formatCurrency(
+                                    item.totalInterest,
+                                    currencyLookup?.[item.formData.loanCurrency]
+                                        ?.locale
+                                )
+                            }}
+                        </td>
+                        <td :class="cellClasses">
+                            <Button
+                                button-type="text"
+                                size="small"
+                                :href="`/calculator/${item.id}`"
+                            >
+                                View
+                            </Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <p
+            v-else
+            class="flex flex-col items-start gap-4"
+        >
+            No history available. Head to the Calculator page to get started.
+            <Button
+                button-type="primary"
+                href="/calculator"
+            >
+                Get Started
             </Button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+        </p>
     </div>
-    <p
-        v-else
-        class="flex flex-col items-start gap-4"
-    >
-      No history available. Head to the Calculator page to get started.
-      <Button
-          button-type="primary"
-          href="/calculator"
-      >
-        Get Started
-      </Button>
-    </p>
-  </div>
 </template>
 
 <script setup>
-import { format } from "date-fns";
-import Button from "@Components/Button.vue";
-import { calculatorHistory } from "@Composables/useCalculator.js";
-import { formatCurrency } from "@Utils/formatCurrency.js";
+import { format } from 'date-fns'
+import Button from '@Components/Button.vue'
+import { calculatorHistory } from '@Composables/useCalculator.js'
+import { formatCurrency } from '@Utils/formatCurrency.js'
 
 // Headers for the table
 const headers = [
-  "Date Created",
-  "Daily Interest (No Margin)",
-  "Daily Interest",
-  "Accrual Date",
-  "No. Days Since Start",
-  "Total Interest",
-  "Actions",
-];
+    'Date Created',
+    'Daily Interest (No Margin)',
+    'Daily Interest',
+    'Accrual Date',
+    'No. Days Since Start',
+    'Total Interest',
+    'Actions'
+]
 
-// Utility function to format dates using date-fns
 const formatDate = (dateStr) => {
-  if (!dateStr) return "";
-  return format(new Date(dateStr), "do MMM yyyy"); // Example: "1st Jan 2025"
-};
+    if (!dateStr) return ''
+    return format(new Date(dateStr), 'do MMM yyyy')
+}
 
 // Abstracted Tailwind classes
-const cellClasses = "border border-zinc-300 px-4 py-2 whitespace-nowrap text-center";
-const headerRowClasses = "bg-gray-200 text-left";
-const rowClasses = "hover:bg-gray-200 even:bg-gray-100 odd:bg-white";
+const cellClasses =
+    'border border-zinc-300 px-4 py-2 whitespace-nowrap text-center'
+const headerRowClasses = 'bg-gray-200 text-left'
+const rowClasses = 'hover:bg-gray-200 even:bg-gray-100 odd:bg-white'
 
 const currencyLookup = {
-  gbp: {
-    locale: "gbp",
-    text: "GBP",
-    symbol: "£",
-  },
-  eur: {
-    locale: "eur",
-    text: "EUR",
-    symbol: "€",
-  },
-  usd: {
-    locale: "usd",
-    text: "USD",
-    symbol: "$",
-  },
-};
+    gbp: {
+        locale: 'gbp',
+        text: 'GBP',
+        symbol: '£'
+    },
+    eur: {
+        locale: 'eur',
+        text: 'EUR',
+        symbol: '€'
+    },
+    usd: {
+        locale: 'usd',
+        text: 'USD',
+        symbol: '$'
+    }
+}
 </script>
